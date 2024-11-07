0555b520357c5a2c7fed86caf76eab4fbfc619ad
linwenjun <linwenjun@163.com>
2024-11-07 15:06:58 +0800

    feat: change ui for input

--- src/app/section-one/section-one.component.ts
+++ src/app/section-one/section-one.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import {FormsModule} from "@angular/forms";
+import {MatFormField} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
+import {MatLabel} from "@angular/material/form-field";
 
 @Component({
   selector: 'app-section-one',
   standalone: true,
   imports: [
-    FormsModule
+    FormsModule,
+    MatFormField,
+    MatInput,
+    MatIconButton,
+    MatIcon,
+    MatLabel
   ],
   templateUrl: './section-one.component.html',
   styleUrl: './section-one.component.css'
